60de64735b50ad5b2d9dfb10a526eec7f8549390
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2020-10-16 21:27:57 +0200

    default and named args

--- src/main/kotlin/no/nav/Functions.kt
+++ src/main/kotlin/no/nav/Functions.kt
@@ -1,11 +1,13 @@
 package no.nav
 
 fun main() {
-    println(basicFunction())
-    println(add(2, 3))
-    println(doStuffWithNumbers(2, 3) { x, y -> x + y })
-    println(adder()(2, 3))
-    println(multiplier(2, 3))
+   println(basicFunction())
+   println(add(2, 3))
+   println(doStuffWithNumbers(2, 3) { x, y -> x + y })
+   println(adder()(2, 3))
+   println(multiplier(2, 3))
+   withManyArgsAndDefault(first = "one", third = "three", second = "two")
+   withManyArgsAndDefault(first = "one", third = "three", second = "two", fourth = "overridden")
 }
 
 fun basicFunction() = listOf("one", "two", "three")
@@ -17,3 +19,6 @@ fun doStuffWithNumbers(x: Int, y: Int, operation: (Int, Int) -> Int)  = operatio
 fun adder() = { x: Int, y: Int -> x + y }
 
 val multiplier = { x: Int, y: Int -> x * y }
+
+fun withManyArgsAndDefault(first: String, second: String, third: String, fourth: String = "four") =
+   println("$first $second $third $fourth")
