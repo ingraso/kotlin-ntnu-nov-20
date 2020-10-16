9d222b816f94f4df95a9fb808cf4b5c5ba558982
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2020-10-16 21:43:04 +0200

    inheritance

--- src/main/kotlin/no/nav/OO.kt
+++ src/main/kotlin/no/nav/OO.kt
@@ -1,13 +1,28 @@
 package no.nav
 
+fun main() {
+   val address = Address("Storgata", 22)
+   val per = Person("Per", address)
+   per.printMe()
+   Circle().draw()
+}
+
 class Person(private val name: String, private val address: Address) {
-    fun printMe() = println("$name lives at $address")
+   fun printMe() = println("$name lives at $address")
 }
 
 data class Address(val street: String, val houseNumber: Int)
 
-fun main() {
-    val address = Address("Storgata", 22)
-    val per = Person("Per", address)
-    per.printMe()
+open class Shape(name: String) {
+   init {
+       println("initialising $name")
+   }
+
+   open fun draw() {}
+}
+
+class Circle(): Shape("circle") {
+   override fun draw() {
+      println("drawing circle")
+   }
 }
